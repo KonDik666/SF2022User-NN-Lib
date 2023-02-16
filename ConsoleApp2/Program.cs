62b72e458589ca6fcffbfdfce6d7eaed05da4792
KonDik666 <system518err@gmail.com>
2023-02-16 11:05:07 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string>();
+            int consultationTime = 30;
+            TimeSpan beginWorkingTime = new TimeSpan(8, 0, 0);
+            TimeSpan endWorkingTime = new TimeSpan(18, 0, 0);
+
+            TimeSpan consultation = new TimeSpan(0, consultationTime, 0);
+
+            while (beginWorkingTime < endWorkingTime)
+            {
+                int j = 0;
+
+                //startTimes[j] += consultation;
+
+
+                TimeSpan newconsultation = beginWorkingTime + consultation;
+                strings.Add($"{beginWorkingTime}-{newconsultation}");
+
+                beginWorkingTime = newconsultation;
+
+            }
+
+
+            Console.WriteLine( strings.ToArray());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SF2022User{NN}Lib/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF2022User_NN_Lib
+{
+    public class Calculations
+    {
+        public string[] AvailablePeriods(TimeSpan[] startTimes,
+int[] durations,
+TimeSpan beginWorkingTime,
+TimeSpan endWorkingTime,
+int consultationTime)
+
+        {
+            List<string> strings= new List<string>();
+
+            // TimeSpan consultationTimeSpan;
+            // consultationTimeSpan.Minutes = consultationTime;
+
+            //startTimes = beginWorkingTime.Minutes + consultationTime
+            TimeSpan consultation = new TimeSpan(0, consultationTime, 0);
+           
+            while (beginWorkingTime < endWorkingTime)
+            {
+                int j = 0;
+                
+                //startTimes[j] += consultation;
+
+
+                TimeSpan newconsultation = beginWorkingTime + consultation;
+                strings.Add($"{beginWorkingTime}-{newconsultation}");
+
+                beginWorkingTime = newconsultation;
+
+            }
+            
+            
+            return strings.ToArray();
+        }
+    }
+}
